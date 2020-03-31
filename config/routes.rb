1e60991890b8bf32b423be1a34b4c386efdd3769
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-03-31 17:42:09 -0300

    campaign tests init

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  root 'pages/home'
+  root 'pages#home'
 
   resources :campaigns, except: [:new] do
     post 'raffle', on: :member
   end
 
-  get 'members/:token/opened', to: 'members/opened'
+  get 'members/:token/opened', to: 'members#opened'
 
   resources :members, only: [:create, :destroy, :update]
 
