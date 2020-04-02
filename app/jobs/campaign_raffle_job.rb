d2de0238c0faf070bc4d27d539b79a54534490ac
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-04-01 22:36:27 -0300

    raffle email

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,20 @@
+class CampaignRaffleJob < ApplicationJob
+  queue_as :emails
+
+  def perform(*args)
+    friend_relations = Raffle.call(campaign: campaign)
+
+    if friend_relations
+      friend_relations.each do |member, secret_friend|
+        member.set_pixel
+
+        CampaignMailer.raffle(campaign, member, secret_friend)
+                      .deliver_now
+      end
+
+      campaign.update(status: :finished)
+    else
+       # Send mail to owner of campaign (desafio)
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,12 @@ module AmigoSecreto
   # Base application.
   class Application < Rails::Application
     config.load_defaults 5.0
+
     config.generators do |generators|
       generators.test_framework :rspec
       generators.factory_bot true
     end
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
