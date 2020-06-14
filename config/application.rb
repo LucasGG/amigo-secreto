a1ac012ee4d9e10a1932c685ec1f7561a441584b
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-06-14 16:46:23 -0300

    more configurations

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,11 @@ Bundler.require(*Rails.groups)
 module AmigoSecreto
   # Base application.
   class Application < Rails::Application
-    config.load_defaults 5.0
+    config.load_defaults 5.2
 
     config.generators do |generators|
       generators.test_framework :rspec
       generators.factory_bot true
     end
-
-    config.active_job.queue_adapter = :sidekiq
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Rails.application.config.active_job.queue_adapter = :sidekiq
+
 Sidekiq.configure_server do |config|
   config.redis = { url: URI.join(ENV.fetch('REDIS_URL'), '/0').to_s }
 end
