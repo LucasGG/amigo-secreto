13cadb389c27c4bca45da4ee327f9cf88db87315
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-06-14 12:13:35 -0300

    more cleanups

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,26 +15,19 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
+    config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=172800' }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'mailcatcher',
-    port: 1025
-  }
+  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+port ENV.fetch('PORT') { 3000 }, ENV.fetch('BINDING') { '127.0.0.1' }
+environment ENV.fetch('RAILS_ENV') { 'development' }
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -8,13 +11,10 @@
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
-port        ENV.fetch('PORT') { 3000 }
-environment ENV.fetch('RAILS_ENV') { 'development' }
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+workers ENV.fetch('WEB_CONCURRENCY') { Etc.nprocessors * 3 / 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
