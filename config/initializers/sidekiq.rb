0ccb096203005b8fe120425d383752df0f4ce8c3
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-06-14 16:35:16 -0300

    use redis server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: URI.join(ENV.fetch('REDIS_URL'), '/0').to_s }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: URI.join(ENV.fetch('REDIS_URL'), '/0').to_s }
 end
